2d8b4736ba2004fe900647045c2f1fdf3d36984c
Sankalp Gaonkar <sankalpgaonkar2000@gmail.com>
2023-04-28 09:29:08 +0530

    made changes

--- Angular Swabhav Techlabs/hello-app/src/app/app.component.ts
+++ Angular Swabhav Techlabs/hello-app/src/app/app.component.ts
@@ -7,16 +7,51 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   // phone ="../assets/phone.jpg";
+  items =["first","second","third"]
+  color:string="red";
+  show:boolean=false; // if the value is true then it will show content in div
+  topBatsmanDetails=[
+     {name:"Virat",matches:200,runs:20000,country:"India"},
+     {name:"Rohit",matches:100,runs:15000,country:"India"},
+     {name:"Surya",matches:90,runs:7000,country:"India"},
+     {name:"Pant",matches:120,runs:12000,country:"India"}
+     ];
 
-  name="";
-  clearName(){
-    this.name="";
-  }
+     scoreDetails=[
+      {name:"Virat",matches:200,lastFiveInnings:[20,30,40,50,30]},
+      {name:"Rohit",matches:100,lastFiveInnings:[20,30,40,50,10]},
+      {name:"Surya",matches:90,lastFiveInnings:[20,30,40,50,20]},
+      {name:"Pant",matches:120,lastFiveInnings:[20,30,40,50,15]}
+    ];
+  // sum=""
+  // a=""
+  // b=""
+  // calculateSum(){
+  //   this.sum=String((parseInt(this.a)||0)+(parseInt(this.b)||0))
+  // }
+  
 
-  test="";
-  clearText(){
-    this.test="";
-  }
+  // getSum(box1:string,box2:string){
+  //   let a = parseInt(box1) || 0;
+  //   let b = parseInt(box2) || 0;
+  //   this.sumDisplay= a + b;
+  // }
+  // paraDisplay:string="";
+
+  // getValue(text:string){
+  //   this.paraDisplay=text;
+  // }
+
+
+  // name="";
+  // clearName(){
+  //   this.name="";
+  // }
+
+  // test="";
+  // clearText(){
+  //   this.test="";
+  // }
   // counter:number = 0;
 
   // resetCounter(){
